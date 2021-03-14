63c8f5551fecad467c2e8f28cc2027200c5de4b7
Gina349 <60369080+Gina349@users.noreply.github.com>
2021-03-14 11:44:03 -0400

    Added Assignment_4

---
+++ Assignments/Assignment_4/Assignment_4/Main.java
@@ -0,0 +1,20 @@
+package Assignment_4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        summation(a);
+    }
+    static int summation(int[] a) {
+        int total = 0;
+        for (int i : a)
+            total += i;
+        System.out.println("This value is: " + total);
+        return total;
+
+
+    }
+}
+
+
